9c2f7d492b95acbf82a3096c8935f290b28527f0
Hannah McCollough <hsmccollough@gmail.com>
2017-06-27 01:05:15 +0000

    Create that was easy button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,6 +1,6 @@
 function sayThatWasEasy() {
-  var thatWasEasy = new Audio("that_was_easy.mp3");
-  thatWasEasy.play();
+    var thatWasEasy = new Audio("that_was_easy.mp3");
+    thatWasEasy.play();
 }
 
 $("#easy").on("click", sayThatWasEasy);
@@ -8,6 +8,7 @@ $("#easy").on("click", sayThatWasEasy);
 $(document).keypress(delegateKeypress);
 
 function delegateKeypress(event) {
-  if (event.charCode == 32) {
-    $("#easy").trigger("click");
-  }
+    if (event.keyCode == 32) {
+        $("#easy").trigger("click");
+    }
+}
